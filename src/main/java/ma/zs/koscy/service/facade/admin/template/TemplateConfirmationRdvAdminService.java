90d9557ce4a13f8f30d2b6c2b207d60056dd25d0
khaoula-AT <khaoulaaitidder2020@gmail.com>
2024-03-27 13:15:26 +0000

    dao:TemplateConfirmationRdv

--- src/main/java/ma/zs/koscy/service/facade/admin/template/TemplateConfirmationRdvAdminService.java
+++ src/main/java/ma/zs/koscy/service/facade/admin/template/TemplateConfirmationRdvAdminService.java
@@ -1,5 +1,9 @@
 package ma.zs.koscy.service.facade.admin.template;
 
-public class TemplateConfirmationRdvAdminService {
+import ma.zs.koscy.bean.core.template.TemplateConfirmationRdv;
+import ma.zs.koscy.dao.criteria.core.template.TemplateConfirmationRdvCriteria;
+import ma.zs.koscy.zynerator.service.IService;
+
+public interface TemplateConfirmationRdvAdminService extends IService<TemplateConfirmationRdv, TemplateConfirmationRdvCriteria> {
 
 }
